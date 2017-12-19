7b862c6e7b9c00e7d333c6d168ed886192e1e1fb
Emre Karahan <e.karahan@jollytur.com>
2017-12-19 17:55:10 +0300

    first commit

---
+++ Dynamo/CreateClass.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+
+namespace Dynamo
+{
+    public class CreateClasses
+    {
+        public static string CreateClass(Dictionary<string, Type> param)
+        {
+            // Create a namespace: (namespace CodeGenerationSample)
+            var @namespace = SyntaxFactory.NamespaceDeclaration(SyntaxFactory.ParseName("CodeGenerationSample"))
+                .NormalizeWhitespace();
+
+            // Add System using statement: (using System)
+            @namespace = @namespace.AddUsings(SyntaxFactory.UsingDirective(SyntaxFactory.ParseName("System")));
+
+            //  Create a class: (class Order)
+            var classDeclaration = SyntaxFactory.ClassDeclaration("Order");
+
+            // Add the public modifier: (public class Order)
+            classDeclaration = classDeclaration.AddModifiers(SyntaxFactory.Token(SyntaxKind.PublicKeyword));
+
+//            // Inherit BaseEntity<T> and implement IHaveIdentity: (public class Order : BaseEntity<T>, IHaveIdentity)
+//            classDeclaration = classDeclaration.AddBaseListTypes(
+//                SyntaxFactory.SimpleBaseType(SyntaxFactory.ParseTypeName("BaseEntity<Order>")),
+//                SyntaxFactory.SimpleBaseType(SyntaxFactory.ParseTypeName("IHaveIdentity")));
+
+//            // Create a string variable: (bool canceled;)
+//            var variableDeclaration = SyntaxFactory.VariableDeclaration(SyntaxFactory.ParseTypeName("bool"))
+//                .AddVariables(SyntaxFactory.VariableDeclarator("canceled"));
+
+//            // Create a field declaration: (private bool canceled;)
+//            var fieldDeclaration = SyntaxFactory.FieldDeclaration(variableDeclaration)
+//                .AddModifiers(SyntaxFactory.Token(SyntaxKind.PrivateKeyword));
+
+            // Create a Property: (public int Quantity { get; set; })
+            
+            
+//            var propertyDeclaration = SyntaxFactory.PropertyDeclaration(SyntaxFactory.ParseTypeName("int"), "Quantity")
+//                .AddModifiers(SyntaxFactory.Token(SyntaxKind.PublicKeyword))
+//                .AddAccessorListAccessors(
+//                    SyntaxFactory.AccessorDeclaration(SyntaxKind.GetAccessorDeclaration)
+//                        .WithSemicolonToken(SyntaxFactory.Token(SyntaxKind.SemicolonToken)),
+//                    SyntaxFactory.AccessorDeclaration(SyntaxKind.SetAccessorDeclaration)
+//                        .WithSemicolonToken(SyntaxFactory.Token(SyntaxKind.SemicolonToken)));
+
+
+            foreach (KeyValuePair<string,Type> item in param)
+            {
+                var propertyDeclaration = SyntaxFactory.PropertyDeclaration(SyntaxFactory.ParseTypeName(item.Value.Name.ToLower()), item.Key)
+                    .AddModifiers(SyntaxFactory.Token(SyntaxKind.PublicKeyword))
+                    .AddAccessorListAccessors(
+                        SyntaxFactory.AccessorDeclaration(SyntaxKind.GetAccessorDeclaration)
+                            .WithSemicolonToken(SyntaxFactory.Token(SyntaxKind.SemicolonToken)),
+                        SyntaxFactory.AccessorDeclaration(SyntaxKind.SetAccessorDeclaration)
+                            .WithSemicolonToken(SyntaxFactory.Token(SyntaxKind.SemicolonToken)));
+
+                classDeclaration = classDeclaration.AddMembers(propertyDeclaration);
+            }
+
+
+            //            // Create a stament with the body of a method.
+            //            var syntax = SyntaxFactory.ParseStatement("canceled = true;");
+
+            //            // Create a method
+            //            var methodDeclaration = SyntaxFactory
+            //                .MethodDeclaration(SyntaxFactory.ParseTypeName("void"), "MarkAsCanceled")
+            //                .AddModifiers(SyntaxFactory.Token(SyntaxKind.PublicKeyword))
+            //                .WithBody(SyntaxFactory.Block(syntax));
+
+            // Add the field, the property and method to the class.
+            //classDeclaration = classDeclaration.AddMembers(fieldDeclaration, propertyDeclaration, methodDeclaration);
+
+            // Add the class to the namespace.
+            @namespace = @namespace.AddMembers(classDeclaration);
+
+            // Normalize and get code as string.
+            var code = @namespace
+                .NormalizeWhitespace()
+                .ToFullString();
+
+            return code;
+        }
+    }
+}
---
+++ Dynamo/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Text;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.Text;
+
+namespace Dynamo
+{
+ public class Program
+    {
+        private static readonly IEnumerable<string> DefaultNamespaces =
+            new[]
+            {
+                "System", 
+                "System.IO", 
+                "System.Net", 
+                "System.Linq", 
+                "System.Text", 
+                "System.Text.RegularExpressions", 
+                "System.Collections.Generic"
+            };
+
+        private static string runtimePath = @"C:\Program Files (x86)\Reference Assemblies\Microsoft\Framework\.NETFramework\v4.6\{0}.dll";
+
+        private static readonly IEnumerable<MetadataReference> DefaultReferences =
+            new[]
+            {
+                MetadataReference.CreateFromFile(string.Format(runtimePath, "mscorlib")),
+                MetadataReference.CreateFromFile(string.Format(runtimePath, "System")),
+                MetadataReference.CreateFromFile(string.Format(runtimePath, "System.Core"))
+            };
+
+        private static readonly CSharpCompilationOptions DefaultCompilationOptions =
+            new CSharpCompilationOptions(OutputKind.WindowsRuntimeApplication)
+                    .WithOverflowChecks(true)
+                    .WithOptimizationLevel(OptimizationLevel.Release)
+                    .WithUsings(DefaultNamespaces);
+
+        public static SyntaxTree Parse(string text, string filename = "", CSharpParseOptions options = null)
+        {
+            var stringText = SourceText.From(text, Encoding.UTF8);
+            return SyntaxFactory.ParseSyntaxTree(stringText, options, filename);
+        }
+
+        public static void Main(string[] args)
+        {
+            //ReferenceFinder finder = new ReferenceFinder();
+            //finder.Find("Read");
+
+            var code = CreateClasses.CreateClass(ClassMetadata());
+            //var fileToCompile = @"C:\Users\..\Documents\Visual Studio 2013\Projects\SignalR_Everything\Program.cs";
+            var source = code;
+            var parsedSyntaxTree = Parse(source, "", CSharpParseOptions.Default.WithLanguageVersion(LanguageVersion.CSharp7));
+
+            var compilation
+                = CSharpCompilation.Create("Test.dll", new SyntaxTree[] { parsedSyntaxTree }, DefaultReferences, DefaultCompilationOptions);
+            try
+            {
+                //compilation = compilation.AddReferences(MetadataReference.CreateFromFile(typeof(object).Assembly.Location));
+                compilation = compilation.WithOptions(new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary));
+
+                var result = compilation.Emit(@"c:\Publish\Test.dll");
+
+                Console.WriteLine(result.Success ? "Sucess!!" : "Failed");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+
+
+            Assembly assembly = Assembly.LoadFile(@"c:\Publish\Test.dll");
+            Type type = assembly.GetType("CodeGenerationSample.Order");
+            var obj = Activator.CreateInstance(type);
+
+
+            var json = "{\"Title\" : \"Emre Karahan\",\"Text\" : \"Yym mncq\",\"Tags\" : \"Very Very Tags\"}";
+
+            obj = Newtonsoft.Json.JsonConvert.DeserializeObject(json, type);
+
+            Console.Read();
+        }
+
+
+        public static  Dictionary<string, Type> ClassMetadata()
+        {
+            //string className = "BlogPost";
+
+            var props = new Dictionary<string, Type>() {
+                { "Title", typeof(string) },
+                { "Text", typeof(string) },
+                { "Tags", typeof(string) }
+                
+            };
+            return props;
+        }
+    }
+
+}
